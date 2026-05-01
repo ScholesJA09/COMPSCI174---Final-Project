2262d34cbf0d39123f5047e6f43317ca22bf6298
moreyr18 <moreyr18@uww.edu>
2026-05-01 17:18:18 -0500

    Update item.h

--- item.h
+++ item.h
@@ -22,4 +22,6 @@ public:
 	void print_item() const;
 	void print_cart() const;
 	void add_item(item items[], item cart[], int& cart_amount, int cart_size);
+	void remove_item(item items[], item cart[], int& cart_amount);  // Stretch #2
+	void sort_cart(item cart[], int cart_amount);                   // Stretch #3
 };
